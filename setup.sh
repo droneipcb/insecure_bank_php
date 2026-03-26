a358f08042e6819f0cf15f7186d8b64b089edfb2
droneipcb <71715301+droneipcb@users.noreply.github.com>
2026-03-26 11:13:02 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#software install
+sudo apt update
+sudo apt install mysql-server -y
+sudo apt install php -y
+sudo apt install apache2 -y
+sudo ufw allow in "Apache"
+sudo apt install git -y
+sudo apt install libapache2-mod-php -y
+sudo apt install php-mysql -y
+
+#create database
